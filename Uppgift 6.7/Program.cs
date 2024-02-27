47568a11f376531b31d161289a35e8b4b717756e
Dijar06 <dize06001@utb.vaxjo.se>
2024-02-27 12:24:39 +0100

    Add project files.

---
+++ Uppgift 6.7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett tal som du vill veta ifall det är ett primtal: ");
+            int tal = int.Parse(Console.ReadLine());
+            Console.WriteLine(Metod(tal));
+        }
+
+        static bool Metod(int tal)
+        {
+            int deltal = 0;
+            for (int i = 2; i < tal; i++)
+            {
+                if (tal % i == 0)
+                {
+                    deltal++;
+                }
+            }
+            if (deltal == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
